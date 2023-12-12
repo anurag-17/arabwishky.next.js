1ea5e51fd2ec762075fbbe757cb2b53bd7f10ce2
Anurag Pandey <anurag.pandey17@gmail.com>
2023-12-12 19:32:17 +0530

    changes

--- app/page.js
+++ app/page.js
@@ -468,8 +468,7 @@ const page = () => {
                        "
                         id="pera3"
                       >
-                        {/* וויסקי 1888 גראן רזרב 700 מ"ל */}
-                      </h1>
+                        וויסקי 1888 גראן רזרב 700 מ"ל</h1>
                       <h1
                         className="text-center lg:text-[14px] xl:text-[22px] xl:leading-[20px] xl:mt-2 2xl:text-[34px] 2xl:leading-[30px] 2xl:mt-2"
                         id="pera4"
